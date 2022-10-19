7bbfd13538e672f398d1b28bdf69f3b0f477953f
Skyarlet <108329248+Skyarlet@users.noreply.github.com>
2022-10-19 20:08:52 +0800

    Create kmp_algorithm.cpp

---
+++ kmp_algorithm.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define max 100000
+int nextv1[max];
+int nextVal[max];
+
+// Firstly, We should know how to find the longest prefix and the longest suffix which are equal to each other
+
+// We need to use two pointers to check the prefix and the suffix of a string
+
+// If the prefix and the suffix are equal, then we go forward until the first element that makes the prefix and the suffix are not equal appears.
+
+// We call the first pointer 'j' to check the suffix and the second pointer 'k' to check the prefix.
+
+// And we need an array to record the position 'k' should return to when the prefix and the suffix are not equal to each other.
+// One tip is that the position of 'k' is firstly recorded as '-1' because if the 'j' suffix is not equal to the 'k' prefix, 'k' will return to the beginning
+
+// What's more, if the length of the string is one, there will be no prefix and suffix, thus we return '-1' to tell you it's wrong.
+
+int CalcLCT(char pattern[])
+{
+  if(strlen(pattern) == 1) return -1;
+  int j = 0;
+  nextv1[0] = -1;
+  int k = nextv1[0];
+  
+  while(j < strlen(pattern)){
+    if(k == -1 || pattern[j] == pattern[k]){
+      j++; k++;
+      nextv1[j] = k;
+    }else{
+      k = nextv1[k];
+    }
+  }
+  return nextv1[strlen(pattern)];
+}
